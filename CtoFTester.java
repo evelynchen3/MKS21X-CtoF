635829f520e52b908e19959b87e0ad869770f8ce
Evelyn Chen <evelynchen@evelyns-air.myfiosgateway.com>
2021-09-23 19:22:35 -0400

    Added comments

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,10 @@
+/**
+celsiusToFahrenheit() should have parameter celsius. celsius will be type
+double. Once run, the fahrenheit equivalent of the value inputted for celsius
+will be returned as a double.
+
+fahrenheitToCelsius() should have parameter fahrenheit. fahrenheit will be
+type double. Once run, the celsius equivalent of the value inputted for
+fahrenheit will be returned as a double.
+**/
 
